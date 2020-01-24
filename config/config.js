9d959763f578d697c01356ccb779b834472be73d
Johan Navarro <21sebas12@gmail.com>
2020-01-24 16:00:21 -0500

    se crea el archivo de configuración para la BD local

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    table_products: 'production_products',
+    aws_local_config: {
+        region: 'local',
+        endpoint: 'http://localhost:8000'
+    },
+    aws_remote_config: {
+    }
+};
